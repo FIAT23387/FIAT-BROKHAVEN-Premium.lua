cd34939def57f180304bd268942a77d7a668d44c
FIAT23387 <kellyancurcio@gmail.com>
2025-09-24 15:51:08 -0300

    Kj
    Zujjsj

--- FIAT BROKHAVEN Premium.lua
+++ FIAT BROKHAVEN Premium.lua
@@ -1,7 +1,1 @@
-warn("Função ativada!")
-local mensagem = "FIAT BROKHAVEN Premium"
-for i = 1, #mensagem do
-    warn(mensagem:sub(1,i))
-    wait(0.1)
-end
-warn("✅ Script carregado com sucesso!")
+
